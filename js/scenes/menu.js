4946e2dd79ae191edcd8fe4e54492822bc3b9d68
satanas <wil.alejandro@gmail.com>
2017-09-02 19:36:08 -0700

    Added module to render text

--- js/scenes/menu.js
+++ js/scenes/menu.js
@@ -3,12 +3,19 @@ var MenuScene = function() {
   _.inherits(Scene);
   Scene.call(_);
 
+  $.text = new Text();
+
   _.update = function() {
     $.x.clr('#fff');
-    $.x.s();
-    $.x.ct('Pandemia', 75, 280, 'red', 'courier');
-
-    $.x.r();
+    //$.x.ct('Pandemia', 125, 290, 'red', 'courier');
+    $.text.draw({
+      y: 180,
+      halign: 'center',
+      text: 'Pandemia',
+      hspacing: 10,
+      scale: 15,
+      color: '#3cdb02'
+    });
 
     if ($.in.p(13)) {
       _.exit();
